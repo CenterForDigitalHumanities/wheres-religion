5d4ff6e1f771cdee27b10aaef6e49fa74e2b3490
Patrick Cuba <cubap@slu.edu>
2022-10-31 14:16:19 -0500

    4 header footer (#6)
    * fonts
    
    * basic pages
    
    * templated shared components
    
    * add icons
    
    * login on main screen
    
    * move scripts
    
    * defer
    
    * only autoformatting
    
    * hide footer on small and add icons
    
    * Nav actions bottom bar
    
    * alternate navbar for small (unfixed)
    
    * css alt
    
    * fix merge
    
    Co-authored-by: Bryan Haberberger <bryan.j.haberberger@slu.edu>

--- js/init.js
+++ js/init.js
@@ -14,12 +14,30 @@ class SiteNav extends HTMLElement {
   }
   connectedCallback() {
     this.innerHTML = `
+    <div class="navbar-fixed hide-on-small-and-down">
     <nav class="light-blue lighten-1" role="navigation">
       <div class="nav-wrapper container"><a id="logo-container" href="/" class="brand-logo"><img src="/css/graphics/logo_clear.png" height="64" alt=""></a>
         <ul class="right hide-on-med-and-down">
-          <li><a href="/dashboard.html">Dashboard</a></li>
-          <li><a href="/entry.html">Entry</a></li>
-          <li><a href="/fieldnotes.html">Fieldnotes</a></li>
+          <li><a href="/dashboard.html">Dashboard<i class="material-icons right">speed</i></a></li>
+          <li><a href="/entry.html">Entry<i class="material-icons right">snippet_folder</i></a></li>
+          <li><a href="/fieldnotes.html">Fieldnotes<i class="material-icons right">edit_note</i></a></li>
+        </ul>
+
+        <ul id="nav-mobile" class="sidenav">
+          <li><a href="/dashboard.html"><i class="material-icons">speed</i>Dashboard</a></li>
+          <li><a href="/entry.html"><i class="material-icons">snippet_folder</i>Entry</a></li>
+          <li><a href="/fieldnotes.html"><i class="material-icons">edit_note</i>Fieldnotes</a></li>
+        </ul>
+        <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      </div>
+    </nav>
+    </div>
+    <nav class="light-blue lighten-1 hide-on-med-and-up" role="navigation">
+      <div class="nav-wrapper container"><a id="logo-container" href="/" class="brand-logo"><img src="/css/graphics/logo_clear.png" height="64" alt=""></a>
+        <ul class="right hide-on-med-and-down">
+          <li><a href="/dashboard.html">Dashboard<i class="material-icons right">speed</i></a></li>
+          <li><a href="/entry.html">Entry<i class="material-icons right">snippet_folder</i></a></li>
+          <li><a href="/fieldnotes.html">Fieldnotes<i class="material-icons right">edit_note</i></a></li>
         </ul>
 
         <ul id="nav-mobile" class="sidenav">
@@ -42,7 +60,8 @@ class SiteFooter extends HTMLElement {
   }
   connectedCallback() {
     this.innerHTML = `
-    <footer class="page-footer deep-purple">
+    <footer class="page-footer deep-purple hide-on-small-and-down">
+
     <div class="container">
       <div class="row">
         <div class="col l6 s12">
@@ -76,7 +95,37 @@ class SiteFooter extends HTMLElement {
       </div>
     </div>
   </footer>
+  <quick-actions></quick-actions>
   `
   }
 }
 customElements.define('site-footer', SiteFooter)
+
+class quickActions extends HTMLElement {
+  constructor() {
+    super()
+  }
+  connectedCallback() {
+    this.innerHTML = `
+    <div class="navbar-fixed hide-on-med-and-up">
+    <nav class="light-blue lighten-1" role="navigation">
+      <div class="nav-wrapper container">
+        <ul class="center">
+          <li><a href="/dashboard.html"><i class="material-icons left">speed</i></a></li>
+        </ul>
+      </div>
+    </nav>
+    </div>
+  `
+  }
+  addAction(label, icon, link) {
+    const action = document.createElement('li')
+    action.innerHTML = `<a href="${link}" title="${label}"><i class="material-icons left">${icon}</i></a>`
+    this.querySelector('ul').append(action)
+  }
+  removeAction(link) {
+    $(`a[href="${link}"]`).parent().remove()
+  }
+}
+customElements.define('quick-actions', quickActions)
+
