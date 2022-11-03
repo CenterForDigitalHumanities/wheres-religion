8f8ae544cd51b42be308cf6b22e032c35546c038
cubap <cubap@slu.edu>
2022-11-03 15:38:10 -0500

    Squashed commit of the following:
    commit 35aafc69abeb6dbc3048181425d3b281cbe83a8e
    Author: Bryan Haberberger <bryan.j.haberberger@slu.edu>
    Date:   Thu Nov 3 11:28:11 2022 -0500
    
        Functioning Media Prototype (#8)
    
        * fonts
    
        * basic pages
    
        * templated shared components
    
        * add icons
    
        * login on main screen
    
        * move scripts
    
        * defer
    
        * only autoformatting
    
        * A prototype of a screen to begin the process of capturing media, followed by a preview with a choice to upload or cancel.
    
        * OK, got it to the point where selecting on the computer puts the information into a preview area.  It says that if you test it on a phone, it will pull up the camera to take a picture.  I am going to deploy this branch.
    
        * When it is an image, generate an image preview element
    
        * niiicceee now make it thumbnail sized
    
        * more merge
    
        * Cleanup, documentation, full functionality
    
        * Meh less words is better
    
        * Give these a little space
    
        * An input may not be selected
    
        * elif reduction
    
        switch on expected filetypes and remove single use assignments.
    
        * uncomplexing
    
        guard clause and less if-else
    
        * whitespace
    
        * move outside conditional
    
        * whitespace
    
        * organizing errors
    
        - mediaRecapture() no longer fails if nothing is selected
        - uploadFile() returns success to avoid else
    
        * empty elements efficiently
    
        * whitespace
    
        * whoops, one bad 'TAB'ing
    
        Co-authored-by: cubap <cubap@slu.edu>
    
    commit 5d4ff6e1f771cdee27b10aaef6e49fa74e2b3490
    Author: Patrick Cuba <cubap@slu.edu>
    Date:   Mon Oct 31 14:16:19 2022 -0500
    
        4 header footer (#6)
    
        * fonts
    
        * basic pages
    
        * templated shared components
    
        * add icons
    
        * login on main screen
    
        * move scripts
    
        * defer
    
        * only autoformatting
    
        * hide footer on small and add icons
    
        * Nav actions bottom bar
    
        * alternate navbar for small (unfixed)
    
        * css alt
    
        * fix merge
    
        Co-authored-by: Bryan Haberberger <bryan.j.haberberger@slu.edu>

---
+++ js/media.js
@@ -0,0 +1,126 @@
+const S3_URI_PREFIX = "https://livedreligion.s3.amazonaws.com/"
+const S3_PROXY_PREFIX = "http://s3-proxy.rerum.io/S3/"
+
+/**
+ * User has chosen and confirmed the file.  Generate the on-screen preview.
+ * @param {ChangeEvent} event
+ */
+function fileSelected(event) {
+    const file = event.target.files[0]
+    if (!file) { return }
+    let fileSize = (file.size > 1024 * 1024)
+        ? (Math.round(file.size * 100 / (1024 * 1024)) / 100).toString() + 'MB'
+        : (Math.round(file.size * 100 / 1024) / 100).toString() + 'KB'
+
+    mediaPreview.querySelector('.fileName').innerHTML = `Name: ${file.name}`
+    mediaPreview.querySelector('.fileSize').innerHTML = `Size: ${fileSize}`
+    mediaPreview.querySelector('.fileType').innerHTML = `Type: ${file.type}`
+
+    let elementType
+    switch (file.type.split("/")[0]) {
+        case "image": elementType = "img"
+            break
+        case "video": elementType = "video"
+            break
+        case "audio": elementType = "audio"
+            break
+        default: elementType = "object"
+    }
+    const reader = new FileReader()
+    reader.onload = (e) => {
+        preview.innerHTML = elementType === "img"
+            ? `<img class="preview" src="${e.target.result}" alt="Captured Image" />`
+            : `<${elementType} class="preview" controls><source src="${e.target.result}" type="${file.type}" </source></${elementType}>`
+    }
+    reader.readAsDataURL(file)
+}
+/**
+ * One of the big div wrapped icons with text was clicked
+ * The clickable element is actually a hidden input.
+ * If the thing clicked was not the hidden input, click the hidden input.
+ * @param {string} which - "camera" or "recorder"
+ * @param {ClickEvent} event
+*/
+function mediaCapture(which, event) {
+    if (event.target.tagName === "INPUT") {
+        return
+    }
+    event.preventDefault()
+    event.stopPropagation()
+    if (which === "camera") {
+        mic_media.classList.remove("selected")
+        cam_media.classList.add("selected")
+        cam_media.click()
+    }
+    else {
+        cam_media.classList.remove("selected")
+        mic_media.classList.add("selected")
+        mic_media.click()
+    }
+}
+
+/**
+ * The user would like to re-capture the same media type as the thing already captured.
+ * This needs to reset the media preview, and open the camera or mic for recapture.
+*/
+function mediaRecapture() {
+    const chosen = document.querySelector("input.selected")
+    if (!chosen) { return }
+    uploadCancelled("Recapture media chosen")
+    chosen.click()
+}
+
+/**
+ * The upload button was clicked.  Upload the file from the selected input.
+ */
+function uploadFile() {
+    mediaPreview.querySelector('.mediastatus').innerHTML = "Uploading, please wait..."
+    const file = document.querySelector("input.selected").files[0]
+    let data = new FormData()
+    data.append('file', file)
+    fetch(S3_PROXY_PREFIX + "uploadFile", {
+        method: "POST",
+        mode: "cors",
+        body: data
+    })
+    .then(resp => {
+        console.log("Got the response from the upload file servlet");
+        if (resp.ok) { return uploadComplete(resp.headers.get("Location")) }
+        resp.text().then(text => uploadFailed(text))
+    })
+    .catch(err => {
+        console.error(err)
+        uploadFailed(err)
+    })
+}
+
+
+/**
+ * The file upload was successful.
+ * @param {string} uri - The URI of the uploaded item
+ */
+function uploadComplete(uri) {
+    mediaPreview.querySelector('.mediastatus').innerHTML = "Upload Complete!"
+}
+
+/**
+ * The file upload failed.  Tell the user
+ */
+function uploadFailed(message = "Upload Failed.") {
+    mediaPreview.querySelector('.mediastatus').innerHTML = message
+}
+
+/**
+ * The file upload was cancelled.  RESET.
+ */
+function uploadCancelled(message = "Upload Cancelled ☹") {
+    mediaPreview.querySelector('.fileName').replaceChildren()
+    mediaPreview.querySelector('.fileSize').replaceChildren()
+    mediaPreview.querySelector('.fileType').replaceChildren()
+    mediaPreview.querySelector('.mediastatus').innerHTML = message
+    setTimeout(() => {
+        mediaPreview.querySelector('.mediastatus').replaceChildren()
+    }, 3000)
+    document.querySelector("input.selected")?.classList.remove("selected")
+    preview.replaceChildren()
+}
