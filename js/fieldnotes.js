bbf6efce7b98b72dcf4d24eff5fe2b9bf84dd840
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2022-11-03 14:36:52 -0500

    Documentation, testing and polishing.  This might be worthy.

--- js/fieldnotes.js
+++ js/fieldnotes.js
@@ -1,27 +1,46 @@
-function submitNote(event){
+/**
+ * Add the "Note Notification" to the queue.
+ * Add the HTML <li> element for this note, and add it to cache. 
+ */  
+function submitNote(){
 	let noteObject = {
 		"id" : "note_"+Date.now(),
 		"type" : "MobileNote",
 		"value" : notes.value
 	}
+	/**
+	 * async and save this "Note Notification" to the queue
+	 * then...
+	 */ 
 	let allNotes = JSON.parse(sessionStorage.getItem("mobile_notes")) ?? []
 	allNotes.push(noteObject)
 	sessionStorage.setItem("mobile_notes", JSON.stringify(allNotes))
-    let noteitemhtml =
-    `<li id=${noteObject.id} class="collection-item">
-        <div>
-            ${notes.value.length > 50 ? notes.value.substring(0, 50)+"..." : notes.value}
-            <i title="Tap here to remove this note." onclick="removeNote('${noteObject.id}')" class="material-icons small dropdown-trigger red-text">remove_circle</i>
-        </div>
-    </li>`
-	addedNotes.innerHTML += noteitemhtml
+    addedNotes.innerHTML +=
+    `
+		<li id=${noteObject.id} class="collection-item">
+		    ${notes.value.length > 50 ? notes.value.substring(0, 50)+"..." : notes.value}
+		    <i title="Tap here to remove this note." onclick="removeNote('${noteObject.id}')" class="material-icons small dropdown-trigger red-text secondary-content">remove_circle</i>
+		</li>
+    `
 }
 
-function cancelNote(event){
+/**
+ * Just the best, I'm keeping it.
+ */ 
+function cancelNote(){
 	notes.value = ""
 }
 
+/**
+ * Remove the "Note Notification" from the queue.
+ * Remove the HTML <li> element for this note, and remove it from cache. 
+ * @param {string} noteID - A unique string (probably a URI) that relates to an HTMLElement 'id' attribute
+ */  
 function removeNote(noteID){
+	/**
+	 * async and clear this "Note Notification" from the queue
+	 * then...
+	 */ 
 	let allNotes = JSON.parse(sessionStorage.getItem("mobile_notes")) ?? []
 	allNotes = allNotes.filter(obj => obj.id !== noteID)
 	document.getElementById(noteID).remove()
