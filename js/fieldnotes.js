5ad2aa0207a7c15570b42c5b97bf939653ba5de8
cubap <cubap@slu.edu>
2022-11-04 10:23:05 -0500

    ? ? => ??

--- js/fieldnotes.js
+++ js/fieldnotes.js
@@ -15,7 +15,7 @@ function submitNote() {
      * async and save this "Note Notification" to the queue
      * then...
      */
-    let allNotes = JSON.parse(sessionStorage.getItem("mobile_notes")) ? ? []
+    let allNotes = JSON.parse(sessionStorage.getItem("mobile_notes")) ?? []
     allNotes.push(noteObject)
     sessionStorage.setItem("mobile_notes", JSON.stringify(allNotes))
     addedNotes.innerHTML +=
@@ -45,7 +45,7 @@ function removeNote(noteID) {
      * async and clear this "Note Notification" from the queue
      * then...
      */
-    let allNotes = JSON.parse(sessionStorage.getItem("mobile_notes")) ? ? []
+    let allNotes = JSON.parse(sessionStorage.getItem("mobile_notes")) ?? []
     allNotes = allNotes.filter(obj => obj.id !== noteID)
     document.getElementById(noteID).remove()
     sessionStorage.setItem("mobile_notes", JSON.stringify(allNotes))
